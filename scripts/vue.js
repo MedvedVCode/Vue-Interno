e9b330858b44ffcc3a11196c3e3205a5678c2e47
Fedorov Denis <ssboshen@yandex.ru>
2023-08-15 00:20:34 +0300

    made blog-details.html, add vue.js & v-for for blog-list in blog.html

---
+++ scripts/vue.js
@@ -0,0 +1,51 @@
+new Vue({
+  el: '#blog-list',
+  data: {
+    blogList: [
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-01.png',
+        tag: 'Kitchen Design',
+        title: 'Let&acute;s Get Solution For Building Construction Work',
+        data: '26 December, 2022',
+      },
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-02.png',
+        tag: 'Living Design',
+        title: 'Low Cost Latest Invented Interior Designing<br>Ideas',
+        data: '22 December, 2022',
+      },
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-03.png',
+        tag: 'Interior Design',
+        title: 'Best For Any Office & Business Interior<br>Solution',
+        data: '25 December, 2022',
+      },
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-04.png',
+        tag: 'Kitchen Design',
+        title: 'Let&acute;s Get Solution For Building Construction Work',
+        data: '26 December, 2022',
+      },
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-05.png',
+        tag: 'Living Design',
+        title: 'Low Cost Latest Invented Interior Designing<br>Ideas',
+        data: '22 December, 2022',
+      },
+      {
+        id: Date.now().toString(),
+        url: '/images/blog-item-img-06.png',
+        tag: 'Interior Design',
+        title: 'Best For Any Office & Business Interior<br>Solution',
+        data: '25 December, 2022',
+      },
+    ],
+  },
+  methods: {
+  },
+});
